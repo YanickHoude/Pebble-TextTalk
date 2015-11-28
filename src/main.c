5deb7acb8f469c1238f2ecc0a7c32883d622923f
YanickHoude <yhoude88@gmail.com>
2015-11-28 11:53:01 -0500

    Conversation Buttons

--- src/main.c
+++ src/main.c
@@ -1,15 +1,37 @@
 #include <pebble.h>
 
-static Window *s_main_window;
+int counter = 0;
+int btstatus = 0;
+  
+static Window *app_main_window;
 
-static TextLayer *s_output_layer;
-   
-static void bt_handler(bool connected) {
-  if (connected) {
-    text_layer_set_text(s_output_layer, "Ready For Conversation");
+static TextLayer *bt_output_layer;
+static TextLayer *start_output_layer;
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  if(btstatus == 1){
+  if(counter == 0){
+      text_layer_set_text(start_output_layer, "Conversation Started");
+    counter++;
   }
   else {
-    text_layer_set_text(s_output_layer, "Not Connected");
+    text_layer_set_text(start_output_layer, "Conversation Ended");
+    counter--;
+  }
+  }else text_layer_set_text(start_output_layer, "Enable Bluetooth");
+}
+
+static void click_config_provider(void *context){
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+}
+static void bt_handler(bool connected) {
+  if (connected) {
+    text_layer_set_text(bt_output_layer, "Ready For Conversation");
+    btstatus++;
+  }
+  else {
+    text_layer_set_text(bt_output_layer, "Not Connected");
+    btstatus--;
   }
 }
 
@@ -17,9 +39,13 @@ static void main_window_load(Window *window){
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
   
-  s_output_layer = text_layer_create(GRect(0,0, window_bounds.size.w, window_bounds.size.h));
-  text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
+  bt_output_layer = text_layer_create(GRect(0,0, window_bounds.size.w, window_bounds.size.h));
+  text_layer_set_text_alignment(bt_output_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(bt_output_layer));
+  
+  start_output_layer = text_layer_create(GRect(0, 150, window_bounds.size.w, window_bounds.size.h));
+  text_layer_set_text_alignment(start_output_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(start_output_layer));
   
   #ifdef PBL_SDK_2
   bt_handler(bluetooth_connection_service_peek());
@@ -29,17 +55,19 @@ static void main_window_load(Window *window){
 }
 
 static void main_window_unload(Window *window){
-  text_layer_destroy(s_output_layer);
+  text_layer_destroy(bt_output_layer);
+  text_layer_destroy(start_output_layer);
 }
 
 static void init() {
-  s_main_window = window_create();
+  app_main_window = window_create();
   
-  window_set_window_handlers(s_main_window, (WindowHandlers){
+  window_set_window_handlers(app_main_window, (WindowHandlers){
     .load = main_window_load, 
     .unload = main_window_unload                        
   });
-  window_stack_push(s_main_window, true);
+  window_set_click_config_provider(app_main_window, click_config_provider);
+  window_stack_push(app_main_window, true);
 
 #ifdef PBL_SDK_2
   bluetooth_connection_service_subscribe(bt_handler);
@@ -51,11 +79,14 @@ connection_service_subscribe((ConnectionHandlers) {
   
  /*app_message_register_inbox_received(inbox);
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
-    window_stack_push(s_main_window, true);*/
+    window_stack_push(s_main_wind
+    
+    
+    ow, true);*/
 }
 
 static void deinit(){
-  window_destroy(s_main_window);
+  window_destroy(app_main_window);
 }
 
 int main(void){
